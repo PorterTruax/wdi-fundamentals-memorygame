f5149f94ac5c04443524bf417935c4900c002a25
Porter Truax <portertruax@Porters-MacBook-Pro.local>
2019-03-09 17:29:47 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,84 @@
-var cardOne = "queen";
-
-var cardTwo = "king";
-
-var cardThree = "queen";
-
-var cardFour = "king";
-
-console.log("User flipped:" + " " + cardOne);
 
 
-console.log("User flipped:" + " " + cardFour);
+//Define 'cards' Array
+var cards = [
 
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+
+{
+	rank: "King",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "King",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
+
+//Define array that will be called on throughout the game.
+//Clicked cards will get pushed to this array.
+var cardsInPlay = [];
+
+
+//This function uses the cardsInPlay array to check for a match 
+checkForMatch = function () {
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert ("You found a match!")
+	} else {
+		alert ("Sorry, try again.")
+	}
+}
+}
+
+//function that is flipping the card to reveal its front
+var flipCard = function () {
+
+var cardId = this.getAttribute("data-id");
+
+this.setAttribute('src', cards[cardId].cardImage);
+
+console.log("User flipped " + cards[cardId].rank);
+
+cardsInPlay.push(cards[cardId].rank);
+
+console.log(cards[cardId].cardImage);
+
+console.log(cards[cardId].suit);
+
+checkForMatch();
+}
+
+
+//createBoard function will refresh the gameboard
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++){
+		//Creating element
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+
+
+
+// 
+
+createBoard();
